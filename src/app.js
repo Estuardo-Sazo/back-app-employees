a798774a74c742b9ab6cbb577cf0df02419b1930
Estuardo-Sazo <estuardosazo1206@gmail.com>
2020-11-27 12:05:59 -0600

    Agregando Cors para recibir peticiones desde otra app!

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const Cors = require('cors');
 const app = express();
 
 app.set('port', process.env.PORT || 4000);
 
+app.use(Cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
